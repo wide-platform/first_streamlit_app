84c0e69a33447bc4403091fe360969a13ac6b256
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-05 12:36:57 +0530

    Update streamlit_app.py
    adding recipies

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,8 @@
+import streamlit
+
+streamlit.title("my parents healthy food")
+streamlit.header(" breakfast menu")
+streamlit.text('dosa','sambar','vadai')
+streamlit.text('rice','dal','sambar')
+streamlit.text('idly','sambar','chutney')
 
