ccdf013dabd6883b4d32e7b40510d6884de5b8c5
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-05 12:38:52 +0530

    Update streamlit_app.py
    changes on recipie ---comma

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title("my parents healthy food")
 streamlit.header(" breakfast menu")
-streamlit.text('dosa','sambar','vadai')
-streamlit.text('rice','dal','sambar')
-streamlit.text('idly','sambar','chutney')
+streamlit.text('doss sambar vadai')
+streamlit.text('rice dal sambar')
+streamlit.text('idl samba chutney')
 
