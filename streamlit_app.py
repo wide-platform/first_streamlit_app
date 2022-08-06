8e5d4125bc6639befd992148a8a00a494beee0e6
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-06 17:00:47 +0530

    Update streamlit_app.py
    quotes mistake

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,7 +12,7 @@ streamlit.text('🥣 soup')
 streamlit.text ('🥗 spinach')
 streamlit.text("🐔 chicken")
 streamlit.text('🥑 avacado')
-streamlit.text('🍞 bread")
+streamlit.text('🍞 bread')
 
 streamlit.header("🍌🥭Special menu item 🥝🍇")
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
