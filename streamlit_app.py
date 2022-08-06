ae3253fa8784b667f8e813e76a47f118d3d9ef97
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-06 16:59:32 +0530

    Update streamlit_app.py
    pandas - csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
+import pandas
 
 streamlit.title("my parents healthy food")
 streamlit.header(" breakfast menu")
@@ -10,7 +11,9 @@ streamlit.header("favorite Food")
 streamlit.text('🥣 soup') 
 streamlit.text ('🥗 spinach')
 streamlit.text("🐔 chicken")
-streamlit.text('🥑 avacado")
+streamlit.text('🥑 avacado')
 streamlit.text('🍞 bread")
 
 streamlit.header("🍌🥭Special menu item 🥝🍇")
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+streamlit.dataframe(my_fruit_list)
