706fe30374ab321de8d78fe8b7977c346036da3f
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-06 17:01:15 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,5 +15,5 @@ streamlit.text('🥑 avacado')
 streamlit.text('🍞 bread')
 
 streamlit.header("🍌🥭Special menu item 🥝🍇")
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
