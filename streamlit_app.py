4dd4280660005088967c283c05e3b4131046d14a
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-12 16:41:21 +0530

    Update streamlit_app.py
    adding header

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,6 +23,8 @@ fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.in
 
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
