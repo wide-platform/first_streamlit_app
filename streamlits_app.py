a693bb2c86fd9b01b2b57638e03160fba6f742da
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-14 16:28:20 +0530

    Update streamlits_app.py
    loc add

--- streamlits_app.py
+++ streamlits_app.py
@@ -23,6 +23,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected=streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Banana'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
 
 streamlit.header("Fruityvice Fruit Advice!")
