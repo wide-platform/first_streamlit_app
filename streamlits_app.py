33a15774f1b424f13978a62db1d1c83efd69aa15
wide-platform <69513210+wide-platform@users.noreply.github.com>
2022-08-14 16:47:29 +0530

    Update streamlits_app.py

--- streamlits_app.py
+++ streamlits_app.py
@@ -36,7 +36,7 @@ try:
     fruityvice_normalized=pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
     
-except URL error as e:
+except URLError as e:
   streamlit.error()
 
 streamlit.stop()
